15fa53f4c6c669c8d36704ec46b0636a2d29340e
tasija03 <117221928+tasija03@users.noreply.github.com>
2024-05-21 21:29:17 +0200

    Update userList.js

--- controllers/userList.js
+++ controllers/userList.js
@@ -2,15 +2,7 @@ const userModel = require('../models/user');
 
 module.exports.displayUser = async function (req, res, next){
 
-    try{
-
-        const userObject = await models.getUser(req.body.ime, req.body.starost);
-
-        return res.render('userList.ejs', {
-            user : userObject
-        });
-    }
-    catch(err){
-        next(err);
-    }
-};
+    const users = await userModel.find();
+    res.render('userList', {korisnik: users});
+    
+}
